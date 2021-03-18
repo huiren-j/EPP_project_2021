48ce47ca292e6e26ae5fafd167b78468835b3d47
huiren-j <s6hyjeon@uni-bonn.de>
2021-03-18 22:16:10 +0100

    Finished management files for figures and tables

---
+++ src/data_management/figures_management.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import econtools.metrics as mt
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from itertools import *
+from tables_management import *
+
+
+def figure1_1(df, alpha, beta):
+    '''Generate beliefs variables(beliefs1, beliefs2, bliefs3) and truth variable
+    
+    Args:
+        df (dataframe)
+        alpha (float)
+        beta (float)
+    Returns:
+        df (dataframe)
+    '''
+
+    df["truth"] = 0
+    t = df.columns.get_loc("truth")
+
+    for k in range(1,4):
+        
+        df["beliefs{0}".format(k)] = np.nan
+        
+        ind = df.columns.get_loc("beliefs{0}".format(k))
+        
+        for i in range(len(df)):
+            if k == 1:
+                df.iloc[i,ind] = df.iloc[i,t]*np.random.normal(0,20)
+            elif k == 2:
+                df.iloc[i, ind] = 50*(1-alpha)+alpha* df.iloc[i,t]+np.random.normal(0,30)
+            else:
+                df.iloc[i, ind] = 50*(1-beta)+beta* df.iloc[i,t]+np.random.normal(0,30)
+                df.iloc[i,t] = (i+1) - math.floor((i+1)/100) * 100
+                
+        df.loc[df["beliefs{0}".format(k)] <= 0, "beliefs{0}".format(k)] = 0
+        df.loc[df["beliefs{0}".format(k)] > 100, "beliefs{0}".format(k)] = 100
+            
+    return df
+
+
+
+def figure1_2(df, name_list):
+    '''Gernrate columns based on *beliefs* variables and *truth* variable
+    
+    Args:
+        df (dataframe)
+        name_list (list)
+    
+    Returns:
+        df (dataframe)
+    '''
+
+    prefix = ["ic_", "is_"]
+    col_list = []
+    
+    for pre in prefix:
+        for name in name_list:
+            df[pre + name] = np.nan
+            col_list.append(pre + name)
+
+    for i in range(len(df)):
+        for col in col_list:
+            idx = df.columns.get_loc(col)
+
+            if col[1] =="c":
+
+                if col[3] == "t":
+                    t = df.columns.get_loc("truth")
+                else:
+                    t = df.columns.get_loc(col[3:])
+
+                df.iloc[i, idx] = 30+df.iloc[i,t]+np.random.normal(0,10)
+
+            else:
+                if col[3] == "t":
+                    t = df.columns.get_loc("truth")
+                else:
+                    t = df.columns.get_loc(col[3:])
+                
+                df.iloc[i, idx] = 100+ 30-df.iloc[i,t]+np.random.normal(0,10)
+    return df
+
+
+def figure4_1(df, x):
+    '''Get lower bound and upper bound values
+
+    Args:
+        df (dataframe)
+        x (string)
+    Returns:
+        lb (float)
+        ub (float)
+    '''
+
+    df_reg = df[df["treat"]=="Control"]
+    des = df_reg[x].describe(percentiles=[.05, .95])
+    lb = des["5%"]
+    ub = des["95%"]
+
+    return lb, ub
---
+++ src/data_management/tables_management.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import econtools.metrics as mt
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from itertools import *
+from figures_management import *
+
+
+def col_names(df, name):
+    '''Repeated framework calculating new variables for regression 
+    Args:
+        df (dataframe)
+        name (string)
+    Returns:
+        df (dataframe)
+    '''
+
+    df["ds_"] = df["ds_" + name]
+    df["ds_educ"] = df["ds_" + name]*df["educ_ave"]
+    df["treat_ds_"] = df["treat_ds_"+name]
+    df["treat_ds_educ"] = df["treat_ds_"+name]*df["educ_ave"]
+    df["treat_educ_used"] = df["treat"]*df["educ_ave"]
+    df["perf_"] = df[name]
+    df["perf_educ"] = df[name]*df["educ_ave"]
+    df["treat_perf"] = df["treat_"+name]
+    df["treat_perf_educ"] = df["treat_"+name]*df["educ_ave"]
+    
+    if name != "ave" and name != "math_engl":
+        df["wb_"+name] = df["wb_"+name]*100
+    
+    return df
+
+def reg_var(df):
+    '''Get dummy variables for regression and define control variables names
+
+    Args:
+        df (dataframe)
+    Returns:
+        df (dataframe)
+        X (list)
+    '''
+
+    def control(ctrl):
+        '''input for map()
+        generate dummy variables name list
+
+        Arg:
+            ctrl (string)
+        return:
+            dummies (list)
+        '''
+
+        dummies = []
+        nonlocal df
+        ctrl_val  = df[ctrl].value_counts().keys().sort_values()
+        for i in ctrl_val:
+            dummies.append(ctrl+"_"+str(i))
+        return dummies
+    
+    ctrl_col = ["std","perf_gap_used","C1_strat"]
+    result = list(map(control,ctrl_col))
+    result = list(chain.from_iterable(result))
+    
+    X = ["treat_perf", "treat", "perf_", "educ_ave_reg", "educ_ave_miss", "female_reg", "female_miss", "primary_resp_fem"] + result
+    
+    df = pd.get_dummies(df,columns = ctrl_col)
+    
+    return df, X
+
+
+def table1_1(df, cond, subject):
+    '''Allocate values under certain condtions in dataframe to certain values
+
+    Arg:
+        df (dataframe)
+        cond (string)
+        subject (list)
+    Return:
+        df (dataframe)
+    '''
+
+    def table1_2(subj):
+        '''input of map()
+
+        Arg: 
+            subj (string)
+        Return:
+            df (dataframe)
+        '''
+        nonlocal df
+        df.loc[(df[cond].isnull()==False) & (df[cond]>0),subj] = 1
+    
+        return df
+    
+    df = list(map(tab1_var_iter,subject))
+    df = pd.concat(df).drop_duplicates()
+    return df
+    
+
+def tabable1_3(df, varlist):
+    '''Change negative values to NaN
+
+    Args:
+        df (dataframe)
+        varlist (list)
+    Return:
+        df (dataframe)
+    '''
+    
+    def table1_4(var):
+        '''input of map()
+        Arg:
+            var (string)
+        Return:
+            df (dataframe)
+        '''
+
+        nonlocal df
+        df.loc[(df[var]<0), var] = np.nan
+        return df
+    
+    df = list(map(tab1_npn_iter, varlist))
+    df = pd.concat(df).drop_duplicates()
+    return df
+
+def table1_max(df, subj, group):
+    '''Taking max value by group
+    Args:
+        df (dataframe)
+        subj (string)
+        group (string)
+
+    Returns:
+        df (dataframe)
+    '''
+    
+    df[subj] = df.groupby([group])[subj].transform(max)
+    return df
+
+def table3_var(df):
+    '''Produce dummy variables and get variables' names
+
+    Args:  
+        df (dataframe)
+    Returns:
+        df (dataframe)
+        ctrl_list (list)
+    '''
+    def control(ctrl):
+        '''input of map
+        Save dummy variables' names
+
+        Args:
+            ctrl (string)
+        Returns:
+            dummies (list)
+        '''
+
+        dummies = []
+        nonlocal df
+        ctrl_val  = df[ctrl].value_counts().keys().sort_values()
+        for i in ctrl_val:
+            dummies.append(ctrl+"_"+str(i))
+        return dummies
+    
+    ctrl_col = ["std","perf_gap_used","C1_strat"]
+    ctrl_list = list(map(control,ctrl_col))
+    ctrl_list = list(chain.from_iterable(ctrl_list))
+    df = pd.get_dummies(df,columns = ctrl_col)
+    
+    return df, ctrl_list
+
+
+
